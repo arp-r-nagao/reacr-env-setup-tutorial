bf6653fcf805263bdde9751b1471a02fc581fe94
ruri-nagao <ruri.nagao.1018@gmail.com>
2025-05-22 11:40:08 +0900

    test

--- test-env-setup-course/src/components/input/TextInput.test.tsx
+++ test-env-setup-course/src/components/input/TextInput.test.tsx
@@ -1,18 +1,18 @@
-import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import TextInput from './TextInput';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TextInput from "./TextInput";
 
-test('TextInput Component test', () => {
+test("TextInput Component test", () => {
   render(<TextInput />);
-  const inputElement = screen.getByRole('textbox');
+  const inputElement = screen.getByRole("textbox");
   expect(inputElement).toBeInTheDocument();
 });
 
-test('TextInput Event Test', async () => {
+test("TextInput Event Testaaa", async () => {
   const user = userEvent.setup();
   render(<TextInput />);
 
-  const inputElement = screen.getByRole('textbox');
-  await user.type(inputElement, 'Hello World');
-  expect(screen.getByText('Hello World')).toBeInTheDocument();
+  const inputElement = screen.getByRole("textbox");
+  await user.type(inputElement, "Hello World");
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
 });
